13731f8685ce702346cbec9664eced6ef3469ca4
Sungbae <sungbae.chun@nu.edu.kz>
2020-12-11 19:57:40 +0600

    Skeleton of magnitude change pruning

--- model.py
+++ model.py
@@ -8,6 +8,47 @@ import matplotlib.pyplot as plt
 import scipy
 
 
+'''
+Should be adapted to do pruning with magnitude change criterion
+'''
+# class FooBarPruningMethod(prune.BasePruningMethod):
+    # """Prune every other entry in a tensor
+    # """
+    # PRUNING_TYPE = 'unstructured'
+#
+    # def compute_mask(self, t, default_mask):
+        # mask = default_mask.clone()
+        # mask.view(-1)[::2] = 0
+        # return mask
+#
+# def foobar_unstructured(module, name):
+    # """Prunes tensor corresponding to parameter called `name` in `module`
+    # by removing every other entry in the tensors.
+    # Modifies module in place (and also return the modified module)
+    # by:
+    # 1) adding a named buffer called `name+'_mask'` corresponding to the
+    # binary mask applied to the parameter `name` by the pruning method.
+    # The parameter `name` is replaced by its pruned version, while the
+    # original (unpruned) parameter is stored in a new parameter named
+    # `name+'_orig'`.
+#
+    # Args:
+        # module (nn.Module): module containing the tensor to prune
+        # name (string): parameter name within `module` on which pruning
+                # will act.
+#
+    # Returns:
+        # module (nn.Module): modified (i.e. pruned) version of the input
+            # module
+#
+    # Examples:
+        # >>> m = nn.Linear(3, 4)
+        # >>> foobar_unstructured(m, name='bias')
+    # """
+    # FooBarPruningMethod.apply(module, name)
+    # return module
+
+
 class SkipGramModel(torch.nn.Module):
 
     def __init__(self, vocab_size, emb_dimension):
@@ -25,14 +66,19 @@ class SkipGramModel(torch.nn.Module):
         prune_method(self.u_embeddings, name='weight', amount=pstep)
         prune_method(self.v_embeddings, name='weight', amount=pstep)
 
-    def fix_state(self,modelpath='/raid/zhassylbekov/sungbae/model/initstate.pth'):
+    def fix_state(self, targetpath='/raid/zhassylbekov/sungbae/model/'):
         cuda_using = next(self.parameters()).is_cuda
         if cuda_using:
             self.cpu()
-            torch.save(self.state_dict(), modelpath)
+            torch.save(self.state_dict(), targetpath+'initstate.pth')
             self.cuda()
         else:
-            torch.save(self.state_dict(), modelpath)
+            torch.save(self.state_dict(), targetpath+'initstate.pth')
+
+    def load_weights(self,targetpath='/raid/zhassylbekov/sungbae/model/'):
+        temp = SkipGramModel(self.vocab_size, self.emb_dimension)
+        temp.load_state_dict(torch.load(targetpath+'initstate.pth'))
+        return temp.u_embeddings.weight, temp.v_embeddings.wegiht
 
     def load_state(self, modelpath='/raid/zhassylbekov/sungbae/model/initstate.pth'):
         umask = dict(self.u_embeddings.named_buffers())['weight_mask'].cpu()
